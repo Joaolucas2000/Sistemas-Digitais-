68257c8c31db3b1e69983bf2b458f88b57cdacf5
Joao Lucas <joao.mouralucas@hotmail.com>
2023-04-01 22:20:20 -0300

    Exercicio 5 adicionado

---
+++ Semana 3/Exercicio 5/plot02.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 =np.arange(-1000,1000,1) #(valor inicial, valor final, pulo)
+plt.plot(x1, x1**2)# permite voce emular uma função 
+plt.show()#sempre tem necessidade dessa função para mostrar
+
+
