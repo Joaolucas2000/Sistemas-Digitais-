c88bf6cbb9b8965aebe7f1b7a05762a079555181
Joao Lucas <joao.mouralucas@hotmail.com>
2023-04-02 18:31:48 -0300

    Adicionado semana 4

---
+++ Semana 4/Exercicio 2/poo01.py
@@ -0,0 +1,9 @@
+
+# POO in python
+
+def hello():
+    print('hello')
+    
+    
+x = 1
+print(type(hello)) 
---
+++ Semana 4/Exercicio 2/poo03.py
@@ -0,0 +1,11 @@
+
+#poo3
+
+class Dog:
+    
+    def bark(self):
+        print('bark')
+        
+d = Dog()
+d.bark()
+print(type(d)) 
